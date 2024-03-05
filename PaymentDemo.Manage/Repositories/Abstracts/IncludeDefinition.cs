7015914555c37f6c897cd5f3347db30694393ab5
HARVEYNASH\huyratio <huynguyen98.clc@gmail.com>
2024-03-05 17:53:14 +0700

    fix bug

--- PaymentDemo.Manage/Repositories/Abstracts/IncludeDefinition.cs
+++ PaymentDemo.Manage/Repositories/Abstracts/IncludeDefinition.cs
@@ -1,14 +1,15 @@
 ﻿using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
+using PaymentDemo.Manage.Entities;
 
 namespace PaymentDemo.Manage.Repositories.Abstracts
 {
-    public abstract class IncludeDefinition<T>
+    public abstract class IncludeDefinition<T> where T : BaseEntity
     {
         public abstract IQueryable<T> Include(IQueryable<T> entities);
     }
 
-    public class IncludeDefinition<T, TProperty> : IncludeDefinition<T>
+    public class IncludeDefinition<T, TProperty> : IncludeDefinition<T> where T : BaseEntity
     {
         public IncludeDefinition(Expression<Func<T, TProperty>> includeEx)
         {
